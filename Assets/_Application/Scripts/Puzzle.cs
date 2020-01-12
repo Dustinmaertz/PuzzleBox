f3e00d4f02f51526c5b96d79ded43a5cfb521696
DustinMaertz <Dustin@objecttheory.com>
2020-01-11 19:55:08 -0800

    Inital commit

---
+++ Assets/_Application/Scripts/Puzzle.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.MixedReality.Toolkit.UI;
+using Microsoft.MixedReality.Toolkit.Input;
+using Microsoft.MixedReality.Toolkit.Utilities;
+
+
+public class Puzzle : MonoBehaviour
+{
+    public GameObject puzzleWhole;
+    public GameObject puzzleParts;
+    public Transform puzzleSpawn;
+    public Transform puzzlePartsGoal;
+    public Transform puzzleGoalLocation;
+    public Transform collectionSpawn;
+    public GridObjectCollection puzzleCollection;
+    public List<Transform> puzzleGoalTransform = new List<Transform>();
+
+    void Start()
+    {
+        // Collect puzzle part goal locations
+        SpawnPuzzleParts();
+
+        // Spawn Ghost version of puzzle object
+        CreatePuzzleGhost();
+    }
+
+    public void SpawnPuzzleParts()
+    {
+        // Clone puzzle pieces for goal transforms
+        foreach (Transform child in puzzleParts.transform)
+        {
+            // Instatiate clone of puzzle part
+            Instantiate(child, collectionSpawn);
+
+            // Remove mesh filter and renderer from puzzle goal transforms 
+            var meshFilter = child.gameObject.GetComponent<MeshFilter>();
+            Destroy(meshFilter);
+            var meshRenderer = child.gameObject.GetComponent<MeshRenderer>();
+            Destroy(meshRenderer);
+        }
+
+        // Add manipulation handeler and mesh collider to puzzle pieces.
+        foreach (Transform child in puzzleCollection.transform)
+        {
+            child.gameObject.AddComponent<BoxCollider>();
+            child.gameObject.AddComponent<ManipulationHandler>();
+            child.gameObject.AddComponent<NearInteractionGrabbable>();
+            child.gameObject.AddComponent<PuzzleGoalLocation>();
+
+            var  puzzleGoal = child.gameObject.GetComponent<PuzzleGoalLocation>();
+            puzzleGoal.GetGoalLocation();
+
+        }
+
+        // Update collection
+        puzzleCollection.UpdateCollection();
+    }
+
+    public void CreatePuzzleGhost()
+    {
+        Instantiate(puzzleWhole, puzzleSpawn);
+        puzzleGoalLocation.position = puzzleSpawn.position;
+    }
+}
---
+++ Assets/_Application/Scripts/PuzzleGoalLocation.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.MixedReality.Toolkit.UI;
+using Microsoft.MixedReality.Toolkit.Input;
+
+
+public class PuzzleGoalLocation : MonoBehaviour
+{
+    public GameObject goalLocation;
+    public float snapPosDistance = 0.05f;
+    public float snapRotDistance = 20.0f;
+    public bool isAtGoal = false;
+
+    public void GetGoalLocation()
+    {
+        var name = this.name;
+        name = name.Remove(name.Length - 7);
+        goalLocation = GameObject.Find(name);
+    }
+
+    void Update()
+    {
+        if (!isAtGoal)
+        {
+            Vector3 targetDir = goalLocation.transform.position - transform.position;
+            float angle = Vector3.Angle(targetDir, transform.up);
+
+            if (angle < snapRotDistance)
+                print("close");
+
+            if ((transform.position - goalLocation.transform.position).sqrMagnitude <= (snapPosDistance * snapPosDistance) && angle < snapRotDistance)
+            {
+
+                    Debug.Log("Matched " + this.name + " Removing Mapitulatioin controls");
+
+                    transform.position = goalLocation.transform.position;
+                    transform.rotation = goalLocation.transform.rotation;
+                    var manip = this.gameObject.GetComponent<ManipulationHandler>();
+                    Destroy(manip);
+                    var nearint = this.gameObject.GetComponent<NearInteractionGrabbable>();
+                    Destroy(nearint);
+                    isAtGoal = true;
+            }
+            
+            
+            /*
+            if ((transform.position - goalLocation.transform.position).sqrMagnitude <= (snapPosDistance * snapPosDistance) && (transform.eulerAngles - goalLocation.transform.eulerAngles).sqrMagnitude <= (snapRotDistance + snapRotDistance))
+            {
+                Debug.Log("Matched " + this.name + " Removing Mapitulatioin controls");
+
+                transform.position = goalLocation.transform.position;
+                var manip = this.gameObject.GetComponent<ManipulationHandler>();
+                Destroy(manip);
+                var nearint = this.gameObject.GetComponent<NearInteractionGrabbable>();
+                Destroy(nearint);
+                isAtGoal = true;
+            }
+            */
+        }
+       // Debug.Log(goalLocation.name);
+    }
+}

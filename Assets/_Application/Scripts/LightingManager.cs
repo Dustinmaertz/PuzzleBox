e39fb155c7796b0f19192b00ec4b9c5ddf779b53
DustinMaertz <Dustin@objecttheory.com>
2020-01-25 16:13:08 -0800

    Moved HDRI materials and textures to resources folder and started working on lighting manager

---
+++ Assets/_Application/Scripts/LightingManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightingManager : MonoBehaviour
+{
+    public Light dirLight;
+    public GameObject dirLightPrefab;
+    public Transform dirLightSpawn;
+    public ReflectionProbe cubemap;
+    public GameObject hdriSpherePrefab;
+
+    [SerializeField]
+    private Cubemap[] hdriTextures;
+    [SerializeField]
+    private Material[] hdriMaterials;
+    private Color dirLightColor;
+    private float dirLightIntensity = 1.0f;
+
+    void Start()
+    {
+        // Gather HDRI textures from Resources folder
+        hdriTextures = Resources.LoadAll<Cubemap>("Textures/HDRI");
+        hdriMaterials = Resources.LoadAll<Material>("Materials/HDRI");
+    }
+
+    void SetupSceneLighting()
+    {
+        Instantiate(dirLight, dirLightSpawn);
+    }
+
+    void SetDirLightInitTransform()
+    {
+        //dirLightPrefab.transform = 
+    }
+
+    public void ChangeCubemap(int cubemapID)
+    {
+        cubemap.customBakedTexture = hdriTextures[cubemapID];
+    }
+
+
+}
---
+++ Assets/_Application/Scripts/PlaceObject.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.Input;
+
+public class PlaceObject : MonoBehaviour
+{
+    public GameObject placementDisc;
+    public GameObject playSpace;
+
+    private bool discPlaced = false;
+    private bool discMoving = false;
+
+    public void SpawnOnPointer(MixedRealityPointerEventData eventData)
+    {
+        if (discPlaced != true)
+        {
+            if (placementDisc != null)
+            {
+                var result = eventData.Pointer.Result;
+                Instantiate(placementDisc, result.Details.Point, Quaternion.LookRotation(result.Details.Normal));
+                discPlaced = true;
+            }
+        }
+    }
+
+    public void UpdateDiscLocation(MixedRealityPointerEventData eventData)
+    {
+        if (discPlaced == true)
+        {
+            discMoving = true;
+            if (discMoving == true)
+            {
+                var result = eventData.Pointer.Result;
+                placementDisc.transform.position = result.Details.Point;
+                placementDisc.transform.rotation = Quaternion.LookRotation(result.Details.Normal);
+            }
+        }
+    }
+}
